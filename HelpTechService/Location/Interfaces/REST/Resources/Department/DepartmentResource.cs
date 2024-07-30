3ba39b70e8deab1283dd7fac9d323288f8ed7d27
aaron acuña <aaronacu_85@hotmail.com>
2024-07-30 16:45:59 -0500

    feat(location): added resources.

---
+++ HelpTechService/Location/Interfaces/REST/Resources/Department/DepartmentResource.cs
@@ -0,0 +1,5 @@
+﻿namespace HelpTechService.Location.Interfaces.REST.Resources.Department
+{
+    public record DepartmentResource
+        (int Id, string Name);
+}
---
+++ HelpTechService/Location/Interfaces/REST/Resources/District/DistrictResource.cs
@@ -0,0 +1,5 @@
+﻿namespace HelpTechService.Location.Interfaces.REST.Resources.District
+{
+    public record DistrictResource(int Id,
+        int DepartmentId, string Name);
+}
