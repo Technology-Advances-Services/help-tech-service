4df99bf70bf2b9397804c22619bbfb1c170a1a51
aaron acuña <aaronacu_85@hotmail.com>
2024-07-30 18:19:25 -0500

    feat(iam): added repositories for consumer and credential.

---
+++ HelpTechService/IAM/Domain/Repositories/IConsumerCredentialRepository.cs
@@ -0,0 +1,12 @@
+﻿using HelpTechService.IAM.Domain.Model.Entities;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.IAM.Domain.Repositories
+{
+    public interface IConsumerCredentialRepository :
+        IBaseRepository<ConsumerCredential>
+    {
+        Task<string?> FindByConsumerIdAsync
+            (int consumerId);
+    }
+}
