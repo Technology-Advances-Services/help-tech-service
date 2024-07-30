801110688005f044ea416b704e6b88a31b525144
aaron acuña <aaronacu_85@hotmail.com>
2024-07-30 16:32:40 -0500

    feat(location): added services for department and district.

---
+++ HelpTechService/Location/Domain/Services/District/IDistrictQueryService.cs
@@ -0,0 +1,13 @@
+﻿using HelpTechService.Location.Domain.Model.Queries.District;
+
+namespace HelpTechService.Location.Domain.Services.District
+{
+    public interface IDistrictQueryService
+    {
+        Task<bool> Handle
+            (GetDistrictByIdQuery query);
+
+        Task<IEnumerable<Model.Aggregates.District>> Handle
+            (GetDistrictsByDepartmentIdQuery query);
+    }
+}
