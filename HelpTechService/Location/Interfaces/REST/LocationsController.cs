9919faee993bdfdedda05616a8895dd35e8ce4bb
aaron acuña <aaronacu_85@hotmail.com>
2024-07-30 16:51:17 -0500

    feat(location): added controller for district and department.

---
+++ HelpTechService/Location/Interfaces/REST/LocationsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using HelpTechService.Location.Domain.Model.Queries.Department;
+using HelpTechService.Location.Domain.Model.Queries.District;
+using HelpTechService.Location.Domain.Services.Department;
+using HelpTechService.Location.Domain.Services.District;
+using HelpTechService.Location.Interfaces.REST.Transform.Department;
+using HelpTechService.Location.Interfaces.REST.Transform.District;
+
+namespace HelpTechService.Location.Interfaces.REST
+{
+    [Route("api/locations/")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class LocationsController
+            (IDepartmentQueryService departmentQueryService,
+            IDistrictQueryService districtQueryService) :
+            ControllerBase
+    {
+        [Route("all-departments")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllDepartments()
+        {
+            var deparments = await departmentQueryService
+                .Handle(new GetAllDepartmentsQuery());
+
+            var deparmentsResource = deparments
+                .Select(DepartmentResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(deparmentsResource);
+        }
+
+        [Route("districts-by-department")]
+        [HttpGet]
+        public async Task<IActionResult> GetDistrictsByDepartmentsId
+            (int departmentId)
+        {
+            var districts = await districtQueryService
+                .Handle(new GetDistrictsByDepartmentIdQuery
+                (departmentId));
+
+            var districtsResource = districts
+                .Select(DistrictResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(districtsResource);
+        }
+    }
+}
