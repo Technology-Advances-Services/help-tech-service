091093915f6187b9f4b5cd2ab2e62831bf41014f
aaron acuña <aaronacu_85@hotmail.com>
2024-07-30 16:30:31 -0500

    feat(location): added repositories for department and district.

---
+++ HelpTechService/Location/Domain/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,9 @@
+﻿using HelpTechService.Location.Domain.Model.Aggregates;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Location.Domain.Repositories
+{
+    public interface IDepartmentRepository :
+        IBaseRepository<Department>
+    { }
+}
---
+++ HelpTechService/Location/Domain/Repositories/IDistrictRepository.cs
@@ -0,0 +1,12 @@
+﻿using HelpTechService.Location.Domain.Model.Aggregates;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Location.Domain.Repositories
+{
+    public interface IDistrictRepository :
+        IBaseRepository<District>
+    {
+        Task<IEnumerable<District>> FindByDepartmentIdAsync
+            (int departmentId);
+    }
+}
