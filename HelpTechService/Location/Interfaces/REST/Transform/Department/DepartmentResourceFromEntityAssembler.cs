80d3f3770a1efc1e4594303db8148587a40871f2
aaron acuña <aaronacu_85@hotmail.com>
2024-07-30 16:48:44 -0500

    feat(location): added transform.

---
+++ HelpTechService/Location/Interfaces/REST/Transform/Department/DepartmentResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using HelpTechService.Location.Interfaces.REST.Resources.Department;
+
+namespace HelpTechService.Location.Interfaces.REST.Transform.Department
+{
+    public class DepartmentResourceFromEntityAssembler
+    {
+        public static DepartmentResource ToResourceFromEntity
+            (Domain.Model.Aggregates.Department entity) =>
+            new(entity.Id, entity.Name);
+    }
+}
---
+++ HelpTechService/Location/Interfaces/REST/Transform/District/DistrictResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using HelpTechService.Location.Interfaces.REST.Resources.District;
+
+namespace HelpTechService.Location.Interfaces.REST.Transform.District
+{
+    public class DistrictResourceFromEntityAssembler
+    {
+        public static DistrictResource ToResourceFromEntity
+            (Domain.Model.Aggregates.District entity) =>
+            new(entity.Id, entity.DepartmentsId, entity.Name);
+    }
+}
