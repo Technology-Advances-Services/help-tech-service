dba7dea29083044e618d07dccb7e137f1df4f369
aaron acuña <aaronacu_85@hotmail.com>
2024-07-30 19:24:03 -0500

    feat(iam): added service for specialty.

---
+++ HelpTechService/IAM/Domain/Services/Specialty/ISpecialtyQueryService.cs
@@ -0,0 +1,13 @@
+﻿using HelpTechService.IAM.Domain.Model.Queries.Specialty;
+
+namespace HelpTechService.IAM.Domain.Services.Specialty
+{
+    public interface ISpecialtyQueryService
+    {
+        Task<IEnumerable<Model.Entities.Specialty>> Handle
+            (GetAllSpecialtiesQuery query);
+
+        Task<Model.Entities.Specialty> Handle
+            (GetSpecialtyByIdQuery query);
+    }
+}
