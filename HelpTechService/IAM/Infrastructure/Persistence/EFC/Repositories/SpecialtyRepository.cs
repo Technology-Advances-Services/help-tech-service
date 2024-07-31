b29f9680920bdfbc16890fa8aed9a4856fe6187f
aaron acuña <aaronacu_85@hotmail.com>
2024-07-31 18:52:47 -0500

    feat(iam): implemented repository for specialty.

---
+++ HelpTechService/IAM/Infrastructure/Persistence/EFC/Repositories/SpecialtyRepository.cs
@@ -0,0 +1,13 @@
+﻿using HelpTechService.IAM.Domain.Model.Entities;
+using HelpTechService.IAM.Domain.Repositories;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace HelpTechService.IAM.Infrastructure.Persistence.EFC.Repositories
+{
+    public class SpecialtyRepository
+        (HelpTechContext context) :
+        BaseRepository<Specialty>(context),
+        ISpecialtyRepository
+    { }
+}
