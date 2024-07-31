1451e02f222915cceb754385ace58dd646569c91
aaron acuña <aaronacu_85@hotmail.com>
2024-07-30 19:26:55 -0500

    feat(iam): added services for criminal record.

---
+++ HelpTechService/IAM/Domain/Services/CriminalRecord/ICriminalRecordCommandService.cs
@@ -0,0 +1,10 @@
+﻿using HelpTechService.IAM.Domain.Model.Commands.CriminalRecord;
+
+namespace HelpTechService.IAM.Domain.Services.CriminalRecord
+{
+    public interface ICriminalRecordCommandService
+    {
+        Task<bool> Handle
+            (AddCriminalRecordToTechnicalCommand command);
+    }
+}
---
+++ HelpTechService/IAM/Domain/Services/CriminalRecord/ICriminalRecordQueryService.cs
@@ -0,0 +1,10 @@
+﻿using HelpTechService.IAM.Domain.Model.Queries.CriminalRecord;
+
+namespace HelpTechService.IAM.Domain.Services.CriminalRecord
+{
+    public interface ICriminalRecordQueryService
+    {
+        Task<IEnumerable<Model.Entities.CriminalRecord>> Handle
+            (GetCriminalsRecordsByTechnicalIdQuery query);
+    }
+}
