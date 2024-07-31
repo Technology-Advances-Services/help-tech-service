cc9237ef773ff518ab5bc7b4d7a994b8b3c81bac
aaron acuña <aaronacu_85@hotmail.com>
2024-07-30 19:52:18 -0500

    fix(iam): completed method for technical credential.

--- HelpTechService/IAM/Application/Internal/CommandServices/TechnicalCredentialCommandService.cs
+++ HelpTechService/IAM/Application/Internal/CommandServices/TechnicalCredentialCommandService.cs
@@ -1,7 +1,28 @@
-﻿namespace HelpTechService.IAM.Application.Internal.CommandServices
+﻿using HelpTechService.IAM.Domain.Model.Commands.TechnicalCredential;
+using HelpTechService.IAM.Domain.Repositories;
+using HelpTechService.IAM.Domain.Services.TechnicalCredential;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.IAM.Application.Internal.CommandServices
 {
     internal class TechnicalCredentialCommandService
+        (ITechnicalCredentialRepository technicalCredentialRepository,
+        IUnitOfWork unitOfWork) :
+        ITechnicalCredentialCommandService
     {
+        public async Task<bool> Handle
+            (AddTechnicalCredentialCommand command)
+        {
+            try
+            {
+                await technicalCredentialRepository
+                    .AddAsync(new(command));
 
+                await unitOfWork.CompleteAsync();
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
     }
 }
