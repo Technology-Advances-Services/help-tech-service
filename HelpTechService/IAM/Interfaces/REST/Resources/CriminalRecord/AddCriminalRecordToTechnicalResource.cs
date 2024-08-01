3cdac02a6702a2fcd6cb7a698e621f43ce7a88f0
aaron acuña <aaronacu_85@hotmail.com>
2024-08-01 11:58:25 -0500

    feat(iam): added resource for criminal record.

---
+++ HelpTechService/IAM/Interfaces/REST/Resources/CriminalRecord/AddCriminalRecordToTechnicalResource.cs
@@ -0,0 +1,5 @@
+﻿namespace HelpTechService.IAM.Interfaces.REST.Resources.CriminalRecord
+{
+    public record AddCriminalRecordToTechnicalResource
+        (int TechnicalId, string FileUrl);
+}
---
+++ HelpTechService/IAM/Interfaces/REST/Resources/CriminalRecord/CriminalRecordResource.cs
@@ -0,0 +1,5 @@
+﻿namespace HelpTechService.IAM.Interfaces.REST.Resources.CriminalRecord
+{
+    public record CriminalRecordResource
+        (int TechnicalId, string FileUrl);
+}
