33737c0ea95b6dcf4a5b37162081e667111c2f68
aaron acuña <aaronacu_85@hotmail.com>
2024-07-31 19:03:39 -0500

    feat(iam): implemented hashing service.

---
+++ HelpTechService/IAM/Infrastructure/Hashing/Argon2id/EncryptionService.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using HelpTechService.IAM.Application.Internal.OutboundServices;
+
+namespace HelpTechService.IAM.Infrastructure.Hashing.Argon2id
+{
+    public class EncryptionService : IEncryptionService
+    {
+        public string CreateSalt()
+        {
+            byte[] buffer = new byte[16];
+
+            using (RandomNumberGenerator rng = RandomNumberGenerator
+                .Create()) rng.GetBytes(buffer);
+
+            return Convert.ToBase64String(buffer);
+        }
+
+        public string HashCode
+            (string code, string salt)
+        {
+            Konscious.Security
+                .Cryptography.Argon2id encryptionCode =
+                new(Encoding.UTF8.GetBytes(code))
+                {
+                    Salt = Encoding.UTF8.GetBytes(salt),
+                    DegreeOfParallelism = 8,
+                    Iterations = 4,
+                    MemorySize = 1024 * 1024
+                };
+
+            return Convert.ToBase64String
+                (encryptionCode.GetBytes(16));
+        }
+
+        public bool VerifyHash
+            (string code, string salt,
+            string hash)
+        {
+            string newHash = HashCode(code, salt);
+
+            return hash.SequenceEqual(newHash);
+        }
+    }
+}
