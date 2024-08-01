280439cc9128bcd96e44475f359ffd10597858d6
aaron acuña <aaronacu_85@hotmail.com>
2024-07-31 19:00:51 -0500

    feat(iam): implemented repositories for technical.

---
+++ HelpTechService/IAM/Infrastructure/Persistence/EFC/Repositories/TechnicalCredentialRepository.cs
@@ -0,0 +1,38 @@
+﻿using HelpTechService.IAM.Domain.Model.Aggregates;
+using HelpTechService.IAM.Domain.Model.Entities;
+using HelpTechService.IAM.Domain.Repositories;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace HelpTechService.IAM.Infrastructure.Persistence.EFC.Repositories
+{
+    public class TechnicalCredentialRepository
+        (HelpTechContext context) :
+        BaseRepository<TechnicalCredential>(context),
+        ITechnicalCredentialRepository
+    {
+        public async Task<string?> FindByTechnicalIdAsync
+            (int technicalId)
+        {
+            Task<TechnicalCredential?> queryAsync = new(() =>
+            {
+                return
+                (from tc in Context.Set<TechnicalCredential>().ToList()
+                 join te in Context.Set<Technical>().ToList()
+                 on tc.TechnicalsId equals te.Id
+                 where te.Id == technicalId &&
+                 te.State == "ACTIVO"
+                 select tc).FirstOrDefault();
+            });
+
+            queryAsync.Start();
+
+            var result = await queryAsync;
+
+            if (result is null)
+                return null;
+
+            return result.Code;
+        }
+    }
+}
---
+++ HelpTechService/IAM/Infrastructure/Persistence/EFC/Repositories/TechnicalRepository.cs
@@ -0,0 +1,13 @@
+﻿using HelpTechService.IAM.Domain.Model.Aggregates;
+using HelpTechService.IAM.Domain.Repositories;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace HelpTechService.IAM.Infrastructure.Persistence.EFC.Repositories
+{
+    public class TechnicalRepository
+        (HelpTechContext context) :
+        BaseRepository<Technical>(context),
+        ITechnicalRepository
+    { }
+}
