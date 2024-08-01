795f66b234bf186a888999cfcfc344613f06dbf1
aaron acuña <aaronacu_85@hotmail.com>
2024-08-01 12:40:38 -0500

    feat(iam): added transform for criminal record.

---
+++ HelpTechService/IAM/Interfaces/REST/Transform/CriminalRecord/AddCriminalRecordToTechnicalCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using HelpTechService.IAM.Domain.Model.Commands.CriminalRecord;
+using HelpTechService.IAM.Interfaces.REST.Resources.CriminalRecord;
+
+namespace HelpTechService.IAM.Interfaces.REST.Transform.CriminalRecord
+{
+    public class AddCriminalRecordToTechnicalCommandFromResourceAssembler
+    {
+        public static AddCriminalRecordToTechnicalCommand ToCommandFromResource
+            (AddCriminalRecordToTechnicalResource resource) =>
+            new(resource.TechnicalId, resource.FileUrl);
+    }
+}
---
+++ HelpTechService/IAM/Interfaces/REST/Transform/CriminalRecord/CriminalRecordResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using HelpTechService.IAM.Interfaces.REST.Resources.CriminalRecord;
+
+namespace HelpTechService.IAM.Interfaces.REST.Transform.CriminalRecord
+{
+    public class CriminalRecordResourceFromEntityAssembler
+    {
+        public static CriminalRecordResource ToResourceFromEntity
+            (Domain.Model.Entities.CriminalRecord entity) =>
+            new(entity.TechnicalsId, entity.FileUrl);
+    }
+}
