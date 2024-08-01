a3b995850b1f3790c3eecadb967224601a873f0f
aaron acuña <aaronacu_85@hotmail.com>
2024-07-31 19:04:48 -0500

    feat(iam): implemented reniec service.

---
+++ HelpTechService/IAM/Infrastructure/Request/ReniecService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using HelpTechService.IAM.Application.Internal.OutboundServices;
+
+namespace HelpTechService.IAM.Infrastructure.Request
+{
+    public class ReniecService
+            (IConfiguration configuration) :
+            IReniecService
+    {
+        public async Task<bool> ValidateDni
+            (int dni)
+        {
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer",
+                configuration["HttpClient:Reniec"]);
+
+            var httpResponseMessage = await httpClient.GetAsync
+                ("https://dniruc.apisperu.com/api/v1/dni/" + dni);
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var result = await httpResponseMessage
+                    .Content.ReadAsStringAsync();
+
+                var contentResult = JsonSerializer
+                    .Deserialize<Dictionary
+                    <string, JsonElement>>(result);
+
+                if (contentResult != null &&
+                    contentResult.TryGetValue
+                    ("success", out var success))
+                    return success.ValueKind == JsonValueKind.True;
+            }
+
+            return false;
+        }
+    }
+}
