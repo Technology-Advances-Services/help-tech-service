3b71aeb71d3885a9da7fa4a613b4e75dc92d48aa
aaron acuña <aaronacu_85@hotmail.com>
2024-08-01 13:11:06 -0500

    feat(iam): added controller for the access.

---
+++ HelpTechService/IAM/Interfaces/REST/AccessController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using HelpTechService.IAM.Domain.Model.Queries.ConsumerCredential;
+using HelpTechService.IAM.Domain.Model.Queries.Specialty;
+using HelpTechService.IAM.Domain.Model.Queries.Technical;
+using HelpTechService.IAM.Domain.Model.Queries.TechnicalCredential;
+using HelpTechService.IAM.Domain.Model.ValueObjects.Credential;
+using HelpTechService.IAM.Domain.Services.Consumer;
+using HelpTechService.IAM.Domain.Services.ConsumerCredential;
+using HelpTechService.IAM.Domain.Services.CriminalRecord;
+using HelpTechService.IAM.Domain.Services.Specialty;
+using HelpTechService.IAM.Domain.Services.Technical;
+using HelpTechService.IAM.Domain.Services.TechnicalCredential;
+using HelpTechService.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using HelpTechService.IAM.Interfaces.REST.Resources.Consumer;
+using HelpTechService.IAM.Interfaces.REST.Resources.ConsumerCredential;
+using HelpTechService.IAM.Interfaces.REST.Resources.CriminalRecord;
+using HelpTechService.IAM.Interfaces.REST.Resources.Technical;
+using HelpTechService.IAM.Interfaces.REST.Resources.TechnicalCredential;
+using HelpTechService.IAM.Interfaces.REST.Resources.User;
+using HelpTechService.IAM.Interfaces.REST.Transform.Consumer;
+using HelpTechService.IAM.Interfaces.REST.Transform.ConsumerCredential;
+using HelpTechService.IAM.Interfaces.REST.Transform.CriminalRecord;
+using HelpTechService.IAM.Interfaces.REST.Transform.Specialty;
+using HelpTechService.IAM.Interfaces.REST.Transform.Technical;
+using HelpTechService.IAM.Interfaces.REST.Transform.TechnicalCredential;
+
+namespace HelpTechService.IAM.Interfaces.REST
+{
+    [Route("api/access/")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Authorize]
+    public class AccessController
+        (ISpecialtyQueryService specialtyQueryService,
+        ITechnicalQueryService technicalQueryService,
+        ITechnicalCommandService technicalCommandService,
+        ITechnicalCredentialCommandService technicalCredentialCommandService,
+        ITechnicalCredentialQueryService technicalCredentialQueryService,
+        ICriminalRecordCommandService criminalRecordCommandService,
+        IConsumerCommandService consumerCommandService,
+        IConsumerCredentialCommandService consumerCredentialCommandService,
+        IConsumerCredentialQueryService consumerCredentialQueryService) :
+        ControllerBase
+    {
+        [Route("all-specialties")]
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAllSpecialties()
+        {
+            var specialties = await specialtyQueryService
+                .Handle(new GetAllSpecialtiesQuery());
+
+            var specialtiesResources = specialties
+                .Select(SpecialtyResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(specialtiesResources);
+        }
+
+        [Route("login")]
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login
+            ([FromBody] UserResource resource)
+        {
+            if (!Enum.TryParse<ECredentialRole>
+                (resource.Role, out var role))
+                return Unauthorized();
+
+            dynamic? result;
+
+            if (role == ECredentialRole.TECNICO)
+                result = await technicalCredentialQueryService
+                    .Handle(new GetTechnicalCredentialByTechnicalIdAndCodeQuery
+                    (resource.Username, resource.Password));
+
+            else if (role == ECredentialRole.CONSUMIDOR)
+                result = await consumerCredentialQueryService
+                    .Handle(new GetConsumerCredentialByConsumerIdAndCodeQuery
+                    (resource.Username, resource.Password));
+
+            else return Unauthorized();
+
+            if (result is null)
+                return BadRequest();
+
+            return Ok(result.Token);
+        }
+
+        [Route("register-technical")]
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> RegisterTechnical
+            ([FromBody] RegisterTechnicalResource resource)
+        {
+            var result = await technicalCommandService
+                .Handle(RegisterTechnicalCommandFromResourceAssembler
+                .ToCommandFromResource(resource));
+
+            if (result is false)
+                return BadRequest();
+
+            result = await technicalCredentialCommandService
+                .Handle(AddTechnicalCredentialCommandFromResourceAssembler
+                .ToCommandFromResource(new AddTechnicalCredentialResource
+                (resource.Id, resource.Code)));
+
+            return Ok(result);
+        }
+
+        [Route("add-criminal-record-to-technical")]
+        [HttpPost]
+        [Authorize("TECNICO")]
+        public async Task<IActionResult> AddCriminalRecordToTechnical
+            ([FromBody] AddCriminalRecordToTechnicalResource resource)
+        {
+            var validation = await technicalQueryService
+                .Handle(new GetTechnicalByIdQuery
+                (resource.TechnicalId));
+
+            if (validation is false)
+                return BadRequest();
+
+            var result = await criminalRecordCommandService
+                .Handle(AddCriminalRecordToTechnicalCommandFromResourceAssembler
+                .ToCommandFromResource(resource));
+
+            if (result is false)
+                return BadRequest();
+
+            return Ok(result);
+        }
+
+        [Route("register-consumer")]
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> RegisterConsumer
+            ([FromBody] RegisterConsumerResource resource)
+        {
+            var result = await consumerCommandService
+                .Handle(RegisterConsumerCommandFromResourceAssembler
+                .ToCommandFromResource(resource));
+
+            if (result is false)
+                return BadRequest();
+
+            result = await consumerCredentialCommandService
+                .Handle(AddConsumerCredentialCommandFromResourceAssembler
+                .ToCommandFromResource(new AddConsumerCredentialResource
+                (resource.Id, resource.Code)));
+
+            return Ok(result);
+        }
+    }
+}
