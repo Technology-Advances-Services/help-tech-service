4637586bad568f48b68f57a832d9a4f8b9b96c61
aaron acuña <aaronacu_85@hotmail.com>
2024-08-01 12:25:52 -0500

    feat(iam): added transform for specialty.

---
+++ HelpTechService/IAM/Interfaces/REST/Transform/Specialty/SpecialtyResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using HelpTechService.IAM.Interfaces.REST.Resources.Specialty;
+
+namespace HelpTechService.IAM.Interfaces.REST.Transform.Specialty
+{
+    public class SpecialtyResourceFromEntityAssembler
+    {
+        public static SpecialtyResource ToResourceFromEntity
+            (Domain.Model.Entities.Specialty entity) =>
+            new(entity.Id, entity.Name);
+    }
+}
