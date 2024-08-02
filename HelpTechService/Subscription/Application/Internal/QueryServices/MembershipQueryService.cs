10cff69c113a39e0b81b36816a7f306d36e75ad9
aaron acuña <aaronacu_85@hotmail.com>
2024-08-02 17:32:33 -0500

    feat(subscription): implemented query service for membership.

---
+++ HelpTechService/Subscription/Application/Internal/QueryServices/MembershipQueryService.cs
@@ -0,0 +1,21 @@
+﻿using HelpTechService.Subscription.Domain.Model.Aggregates;
+using HelpTechService.Subscription.Domain.Model.Queries.Membership;
+using HelpTechService.Subscription.Domain.Repositories;
+using HelpTechService.Subscription.Domain.Services.Membership;
+
+namespace HelpTechService.Subscription.Application.Internal.QueryServices
+{
+    public class MembershipQueryService
+        (IMembershipRepository membershipRepository) :
+        IMembershipQueryService
+    {
+        public async Task<IEnumerable<Membership>> Handle
+            (GetAllMembershipsQuery query) =>
+            await membershipRepository.ListAsync();
+
+        public async Task<Membership?> Handle
+            (GetMembershipByIdQuery query) =>
+            await membershipRepository
+            .FindByIdAsync(query.Id);
+    }
+}
