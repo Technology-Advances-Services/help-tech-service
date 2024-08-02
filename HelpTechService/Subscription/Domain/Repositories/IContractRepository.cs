77df9d957f33edcd136f0f207ed7af7cc5505f34
aaron acuña <aaronacu_85@hotmail.com>
2024-08-02 17:07:15 -0500

    feat(subscription): added repositories for contract and membership.

---
+++ HelpTechService/Subscription/Domain/Repositories/IContractRepository.cs
@@ -0,0 +1,10 @@
+﻿using HelpTechService.Subscription.Domain.Model.Aggregates;
+
+namespace HelpTechService.Subscription.Domain.Repositories
+{
+    public interface IContractRepository
+    {
+        Task<IEnumerable<Contract>> FindByTechnicalIdAsync
+            (int technicalId);
+    }
+}
---
+++ HelpTechService/Subscription/Domain/Repositories/IMembershipRepository.cs
@@ -0,0 +1,9 @@
+﻿using HelpTechService.Shared.Domain.Repositories;
+using HelpTechService.Subscription.Domain.Model.Aggregates;
+
+namespace HelpTechService.Subscription.Domain.Repositories
+{
+    public interface IMembershipRepository :
+        IBaseRepository<Membership>
+    { }
+}
