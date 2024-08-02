e3b4cfb21426a7f85f39c2e1643d06bf34ac5fe0
aaron acuña <aaronacu_85@hotmail.com>
2024-08-02 17:30:32 -0500

    feat(subscription): implemented query service for contract.

---
+++ HelpTechService/Subscription/Application/Internal/QueryServices/ContractQueryService.cs
@@ -0,0 +1,18 @@
+﻿using HelpTechService.Subscription.Domain.Model.Aggregates;
+using HelpTechService.Subscription.Domain.Model.Queries.Contract;
+using HelpTechService.Subscription.Domain.Repositories;
+using HelpTechService.Subscription.Domain.Services.Contract;
+
+namespace HelpTechService.Subscription.Application.Internal.QueryServices
+{
+    public class ContractQueryService
+        (IContractRepository contractRepository) :
+        IContractQueryService
+    {
+        public async Task<IEnumerable<Contract>> Handle
+            (GetContractsByTechnicalIdQuery query) =>
+            await contractRepository
+            .FindByTechnicalIdAsync
+            (query.TechnicalId);
+    }
+}
