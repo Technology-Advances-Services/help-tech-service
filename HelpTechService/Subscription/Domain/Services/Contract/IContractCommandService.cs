38c32aa6cc40d751e5a680eb4e588d5a877cc5d2
aaron acuña <aaronacu_85@hotmail.com>
2024-08-02 17:13:13 -0500

    feat(subscription): added services for contract.

---
+++ HelpTechService/Subscription/Domain/Services/Contract/IContractCommandService.cs
@@ -0,0 +1,10 @@
+﻿using HelpTechService.Subscription.Domain.Model.Commands.Contract;
+
+namespace HelpTechService.Subscription.Domain.Services.Contract
+{
+    public interface IContractCommandService
+    {
+        Task<bool> Handle
+            (CreateContractCommand command);
+    }
+}
---
+++ HelpTechService/Subscription/Domain/Services/Contract/IContractQueryService.cs
@@ -0,0 +1,10 @@
+﻿using HelpTechService.Subscription.Domain.Model.Queries.Contract;
+
+namespace HelpTechService.Subscription.Domain.Services.Contract
+{
+    public interface IContractQueryService
+    {
+        Task<IEnumerable<Model.Aggregates.Contract>> Handle
+            (GetContractsByTechnicalIdQuery query);
+    }
+}
