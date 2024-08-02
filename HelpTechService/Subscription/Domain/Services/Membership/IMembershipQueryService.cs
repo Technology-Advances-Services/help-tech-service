9c9317fb70559402e8d3af33105bff5857afe9d8
aaron acuña <aaronacu_85@hotmail.com>
2024-08-02 17:23:35 -0500

    feat(subscription): added services for membership.

---
+++ HelpTechService/Subscription/Domain/Services/Membership/IMembershipQueryService.cs
@@ -0,0 +1,13 @@
+﻿using HelpTechService.Subscription.Domain.Model.Queries.Membership;
+
+namespace HelpTechService.Subscription.Domain.Services.Membership
+{
+    public interface IMembershipQueryService
+    {
+        Task<IEnumerable<Model.Aggregates.Membership>> Handle
+            (GetAllMembershipsQuery query);
+
+        Task<Model.Aggregates.Membership?> Handle
+            (GetMembershipByIdQuery query);
+    }
+}
