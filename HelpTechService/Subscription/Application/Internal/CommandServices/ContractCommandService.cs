8db007d8cf0e4351ae375d9b6d5b311c71538b13
aaron acuña <aaronacu_85@hotmail.com>
2024-08-02 17:59:35 -0500

    feat(subscription): implemented external iam service for contract.

--- HelpTechService/Subscription/Application/Internal/CommandServices/ContractCommandService.cs
+++ HelpTechService/Subscription/Application/Internal/CommandServices/ContractCommandService.cs
@@ -1,4 +1,5 @@
-﻿using HelpTechService.Subscription.Domain.Model.Commands.Contract;
+﻿using HelpTechService.Subscription.Application.Internal.OutboundServices.ACL;
+using HelpTechService.Subscription.Domain.Model.Commands.Contract;
 using HelpTechService.Subscription.Domain.Repositories;
 using HelpTechService.Subscription.Domain.Services.Contract;
 using HelpTechService.Shared.Domain.Repositories;
@@ -7,18 +8,40 @@ namespace HelpTechService.Subscription.Application.Internal.CommandServices
 {
     public class ContractCommandService
         (IContractRepository contractRepository,
-        IUnitOfWork unitOfWork) :
+        IUnitOfWork unitOfWork,
+        ExternalIamService externalIamService) :
         IContractCommandService
     {
         public async Task<bool> Handle
             (CreateContractCommand command)
         {
+            bool result;
+
+            if (int.TryParse(command
+                .TechnicalId.ToString(),
+                out int technicalId))
+                result = await externalIamService
+                    .ExistsTechnicalById
+                    (technicalId);
+
+            else if (int.TryParse(command
+                .ConsumerId.ToString(),
+                out int consumerId))
+                result = await externalIamService
+                    .ExistsConsumerById
+                    (consumerId);
+
+            else return false;
+
+            if (result is false)
+                return false;
+
             await contractRepository
                 .AddAsync(new(command));
 
             await unitOfWork.CompleteAsync();
 
-            return false;
+            return true;
         }
     }
 }
---
+++ HelpTechService/Subscription/Application/Internal/OutboundServices/ACL/ExternalIamService.cs
@@ -0,0 +1,16 @@
+﻿using HelpTechService.IAM.Interfaces.ACL;
+
+namespace HelpTechService.Subscription.Application.Internal.OutboundServices.ACL
+{
+    public class ExternalIamService
+        (IIamContextFacade iamContextFacade)
+    {
+        public async Task<bool> ExistsTechnicalById
+            (int id) => await iamContextFacade
+            .ExistsTechnicalById(id);
+
+        public async Task<bool> ExistsConsumerById
+            (int id) => await iamContextFacade
+            .ExistsConsumerById(id);
+    }
+}
