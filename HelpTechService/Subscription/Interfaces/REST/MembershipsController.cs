44cf4b4cc88fc3ce68081184f6d55a417e2123c8
aaron acuña <aaronacu_85@hotmail.com>
2024-08-04 15:32:33 -0500

    feat(subscription): added controller for membership.

---
+++ HelpTechService/Subscription/Interfaces/REST/MembershipsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using HelpTechService.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using HelpTechService.Subscription.Domain.Model.Queries.Membership;
+using HelpTechService.Subscription.Domain.Services.Membership;
+using HelpTechService.Subscription.Interfaces.REST.Transform.Membership;
+
+namespace HelpTechService.Subscription.Interfaces.REST
+{
+    [Route("api/memberships/")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Authorize]
+    public class MembershipsController
+        (IMembershipQueryService membershipQueryService) :
+        ControllerBase
+    {
+        [Route("all-memberships")]
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAllMemberships()
+        {
+            var memberships = await membershipQueryService
+                .Handle(new GetAllMembershipsQuery());
+
+            var membershipsResource = memberships.Select
+                (MembershipResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(membershipsResource);
+        }
+
+        [Route("membership-by-id")]
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetMembershipById
+            ([FromQuery] int id)
+        {
+            var membership = await membershipQueryService
+                .Handle(new GetMembershipByIdQuery(id));
+
+            if (membership is null)
+                return BadRequest();
+
+            var membershipResource = MembershipResourceFromEntityAssembler
+                .ToResourceFromEntity(membership);
+
+            return Ok(membershipResource);
+        }
+    }
+}
