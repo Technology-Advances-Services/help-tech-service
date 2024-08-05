bcc9f738e738cb6c055973c6e34e50835c55fb73
aaron acuña <aaronacu_85@hotmail.com>
2024-08-05 18:24:22 -0500

    feat(report): implemented command and query service for complaint.

---
+++ HelpTechService/Report/Application/Internal/CommandServices/ComplaintCommandService.cs
@@ -0,0 +1,28 @@
+﻿using HelpTechService.Report.Domain.Model.Commands.Complaint;
+using HelpTechService.Report.Domain.Repositories;
+using HelpTechService.Report.Domain.Services.Complaint;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Report.Application.Internal.CommandServices
+{
+    internal class ComplaintCommandService
+        (IComplaintRepository complaintRepository,
+        IUnitOfWork unitOfWork) :
+        IComplaintCommandService
+    {
+        public async Task<bool> Handle
+            (RegisterComplaintCommand command)
+        {
+            try
+            {
+                await complaintRepository
+                    .AddAsync(new(command));
+
+                await unitOfWork.CompleteAsync();
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
---
+++ HelpTechService/Report/Application/Internal/QueryServices/ComplaintQueryService.cs
@@ -0,0 +1,25 @@
+﻿using HelpTechService.Report.Domain.Model.Aggregates;
+using HelpTechService.Report.Domain.Model.Queries.Complaint;
+using HelpTechService.Report.Domain.Repositories;
+using HelpTechService.Report.Domain.Services.Complaint;
+
+namespace HelpTechService.Report.Application.Internal.QueryServices
+{
+    internal class ComplaintQueryService
+        (IComplaintRepository complaintRepository) :
+        IComplaintQueryService
+    {
+        public async Task<Complaint?> Handle
+            (GetComplaintsByJobIdAndSenderQuery query) =>
+            await complaintRepository
+            .FindByJobIdAndSender
+            (query.JobId, query.ComplaintSender);
+
+        public async Task<Complaint?> Handle
+            (GetComplaintsByJobIdAndSenderAndStateQuery query) =>
+            await complaintRepository
+            .FindByJobIdAndSenderAndState
+            (query.JobId, query.ComplaintSender,
+                query.ComplaintState);
+    }
+}
