96c14008db664648afa3227f2344b9e6ae069c61
aaron acuña <aaronacu_85@hotmail.com>
2024-08-05 18:26:41 -0500

    feat(report): implemented query service for type complaint.

---
+++ HelpTechService/Report/Application/Internal/QueryServices/TypeComplaintQueryService.cs
@@ -0,0 +1,21 @@
+﻿using HelpTechService.Report.Domain.Model.Entities;
+using HelpTechService.Report.Domain.Model.Queries.TypeComplaint;
+using HelpTechService.Report.Domain.Repositories;
+using HelpTechService.Report.Domain.Services.TypeComplaint;
+
+namespace HelpTechService.Report.Application.Internal.QueryServices
+{
+    internal class TypeComplaintQueryService
+        (ITypeComplaintRepository typeComplaintRepository) :
+        ITypeComplaintQueryService
+    {
+        public async Task<IEnumerable<TypeComplaint>> Handle
+            (GetAllTypesComplaintsQuery query) =>
+            await typeComplaintRepository.ListAsync();
+
+        public async Task<TypeComplaint?> Handle
+            (GetTypeComplaintByIdQuery query) =>
+            await typeComplaintRepository
+            .FindByIdAsync(query.Id);
+    }
+}
