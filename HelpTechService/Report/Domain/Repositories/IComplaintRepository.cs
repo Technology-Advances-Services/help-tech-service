8598352480ba29b1b3d40eac43f1c13dca1f545d
aaron acuña <aaronacu_85@hotmail.com>
2024-08-05 18:06:01 -0500

    feat(report): added repositories for complaint and type complaint.

---
+++ HelpTechService/Report/Domain/Repositories/IComplaintRepository.cs
@@ -0,0 +1,17 @@
+﻿using HelpTechService.Report.Domain.Model.Aggregates;
+using HelpTechService.Report.Domain.Model.ValueObjects.Complaint;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Report.Domain.Repositories
+{
+    public interface IComplaintRepository :
+        IBaseRepository<Complaint>
+    {
+        Task<Complaint?> FindByJobIdAndSenderAndState
+            (int jobId, EComplaintSender complaintSender,
+            EComplaintState complaintState);
+
+        Task<Complaint?> FindByJobIdAndSender
+            (int jobId, EComplaintSender complaintSender);
+    }
+}
---
+++ HelpTechService/Report/Domain/Repositories/ITypeComplaintRepository.cs
@@ -0,0 +1,9 @@
+﻿using HelpTechService.Report.Domain.Model.Entities;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Report.Domain.Repositories
+{
+    public interface ITypeComplaintRepository :
+        IBaseRepository<TypeComplaint>
+    { }
+}
