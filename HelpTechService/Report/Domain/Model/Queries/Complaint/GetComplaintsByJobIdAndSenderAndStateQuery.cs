d682b85853146dc872e0de735ab435d30c41bbc4
aaron acuña <aaronacu_85@hotmail.com>
2024-08-05 18:00:14 -0500

    feat(report): added queries for complaint.

---
+++ HelpTechService/Report/Domain/Model/Queries/Complaint/GetComplaintsByJobIdAndSenderAndStateQuery.cs
@@ -0,0 +1,8 @@
+﻿using HelpTechService.Report.Domain.Model.ValueObjects.Complaint;
+
+namespace HelpTechService.Report.Domain.Model.Queries.Complaint
+{
+    public record GetComplaintsByJobIdAndSenderAndStateQuery
+        (int JobId, EComplaintSender ComplaintSender,
+        EComplaintState ComplaintState);
+}
---
+++ HelpTechService/Report/Domain/Model/Queries/Complaint/GetComplaintsByJobIdAndSenderQuery.cs
@@ -0,0 +1,7 @@
+﻿using HelpTechService.Report.Domain.Model.ValueObjects.Complaint;
+
+namespace HelpTechService.Report.Domain.Model.Queries.Complaint
+{
+    public record GetComplaintsByJobIdAndSenderQuery
+        (int JobId, EComplaintSender ComplaintSender);
+}
