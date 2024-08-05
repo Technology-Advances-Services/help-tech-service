9a31a305355925b71a19cb7060a65f1d1e1ed521
aaron acuña <aaronacu_85@hotmail.com>
2024-08-05 18:17:12 -0500

    feat(report): added query service for type complaint.

---
+++ HelpTechService/Report/Domain/Services/TypeComplaint/ITypeComplaintQueryService.cs
@@ -0,0 +1,13 @@
+﻿using HelpTechService.Report.Domain.Model.Queries.TypeComplaint;
+
+namespace HelpTechService.Report.Domain.Services.TypeComplaint
+{
+    public interface ITypeComplaintQueryService
+    {
+        Task<IEnumerable<Model.Entities.TypeComplaint>> Handle
+            (GetAllTypesComplaintsQuery query);
+
+        Task<Model.Entities.TypeComplaint?> Handle
+            (GetTypeComplaintByIdQuery query);
+    }
+}
