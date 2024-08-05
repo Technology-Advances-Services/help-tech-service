f51fcac823990e473f69bb190db21d1c575e521a
aaron acuña <aaronacu_85@hotmail.com>
2024-08-05 18:58:11 -0500

    feat(report): added transform for type complaint.

---
+++ HelpTechService/Report/Interfaces/REST/Transform/TypeComplaint/TypeComplaintResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using HelpTechService.Report.Interfaces.REST.Resources.TypeComplaint;
+
+namespace HelpTechService.Report.Interfaces.REST.Transform.TypeComplaint
+{
+    public class TypeComplaintResourceFromEntityAssembler
+    {
+        public static TypeComplaintResource ToResourceFromEntity
+            (Domain.Model.Entities.TypeComplaint entity) =>
+            new(entity.Id, entity.Name);
+    }
+}
