12d83299fe2d6199f4189aa48d23923cd0037fb0
aaron acuña <aaronacu_85@hotmail.com>
2024-08-06 18:55:44 -0500

    feat(interaction): implemented query service for chat member.

---
+++ HelpTechService/Interaction/Application/Internal/QueryServices/ChatMemberQueryService.cs
@@ -0,0 +1,24 @@
+﻿using HelpTechService.Interaction.Domain.Model.Aggregates;
+using HelpTechService.Interaction.Domain.Model.Queries.ChatMember;
+using HelpTechService.Interaction.Domain.Repositories;
+using HelpTechService.Interaction.Domain.Services.ChatMember;
+
+namespace HelpTechService.Interaction.Application.Internal.QueryServices
+{
+    internal class ChatMemberQueryService
+        (IChatMemberRepository chatMemberRepository) :
+        IChatMemberQueryService
+    {
+        public async Task<IEnumerable<ChatMember>> Handle
+            (GetChatMembersByTechnicalIdQuery query) =>
+            await chatMemberRepository
+            .FindByTechnicalIdAsync
+            (query.TechnicalId);
+
+        public async Task<IEnumerable<ChatMember>> Handle
+            (GetChatMembersByConsumerIdQuery query) =>
+            await chatMemberRepository
+            .FindByConsumerIdAsync
+            (query.ConsumerId);
+    }
+}
