49f4eb73a8175955716eb9e677a8e8e9b58fd2cf
aaron acuña <aaronacu_85@hotmail.com>
2024-08-05 19:03:33 -0500

    feat(report): implemented repository for complaint.

---
+++ HelpTechService/Report/Infrastructure/Persistence/EFC/Repositories/ComplaintRepository.cs
@@ -0,0 +1,50 @@
+﻿using HelpTechService.Report.Domain.Model.Aggregates;
+using HelpTechService.Report.Domain.Model.ValueObjects.Complaint;
+using HelpTechService.Report.Domain.Repositories;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace HelpTechService.Report.Infrastructure.Persistence.EFC.Repositories
+{
+    internal class ComplaintRepository
+        (HelpTechContext context) :
+        BaseRepository<Complaint>(context),
+        IComplaintRepository
+    {
+        public async Task<Complaint?> FindByJobIdAndSender
+            (int jobId, EComplaintSender complaintSender)
+        {
+            Task<Complaint?> queryAsync = new(() =>
+            {
+                return
+                (from co in Context.Set<Complaint>().ToList()
+                 where co.JobsId == jobId &&
+                 co.Sender == complaintSender.ToString()
+                 select co).FirstOrDefault();
+            });
+
+            queryAsync.Start();
+
+            return await queryAsync;
+        }
+
+        public async Task<Complaint?> FindByJobIdAndSenderAndState
+            (int jobId, EComplaintSender complaintSender,
+            EComplaintState complaintState)
+        {
+            Task<Complaint?> queryAsync = new(() =>
+            {
+                return
+                (from co in Context.Set<Complaint>().ToList()
+                 where co.JobsId == jobId &&
+                 co.Sender == complaintSender.ToString() &&
+                 co.State == complaintState.ToString()
+                 select co).FirstOrDefault();
+            });
+
+            queryAsync.Start();
+
+            return await queryAsync;
+        }
+    }
+}
