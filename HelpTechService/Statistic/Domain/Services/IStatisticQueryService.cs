0a1e266d8383232a26b4c13ce9015d2df93dba68
aaron acuña <aaronacu_85@hotmail.com>
2024-08-06 15:04:49 -0500

    feat(statistic): added query service.

---
+++ HelpTechService/Statistic/Domain/Services/IStatisticQueryService.cs
@@ -0,0 +1,16 @@
+﻿using HelpTechService.Statistic.Domain.Model.Queries;
+
+namespace HelpTechService.Statistic.Domain.Services
+{
+    public interface IStatisticQueryService
+    {
+        Task<dynamic?> Handle
+            (GetGeneralTechnicalStatisticQuery query);
+
+        Task<dynamic?> Handle
+            (GetDetailedTechnicalStatisticQuery query);
+
+        Task<dynamic?> Handle
+            (GetReviewStatisticQuery query);
+    }
+}
