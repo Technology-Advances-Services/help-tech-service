75bd0e0eb83e67e99fc0d3133195e568918a16f6
aaron acuña <aaronacu_85@hotmail.com>
2024-08-06 18:58:24 -0500

    feat(interaction): added external iam service.

---
+++ HelpTechService/Interaction/Application/Internal/OutboundServices/ACL/ExternalIamService.cs
@@ -0,0 +1,16 @@
+﻿using HelpTechService.IAM.Interfaces.ACL;
+
+namespace HelpTechService.Interaction.Application.Internal.OutboundServices.ACL
+{
+    internal class ExternalIamService
+        (IIamContextFacade iamContextFacade)
+    {
+        public async Task<bool> ExistsTechnicalById
+            (int id) => await iamContextFacade
+            .ExistsTechnicalById(id);
+
+        public async Task<bool> ExistsConsumerById
+            (int id) => await iamContextFacade
+            .ExistsConsumerById(id);
+    }
+}
