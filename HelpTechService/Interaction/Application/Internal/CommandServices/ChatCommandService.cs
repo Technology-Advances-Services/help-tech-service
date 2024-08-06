ade518d388ac1703fd2b036d9bb7a64f41be251f
aaron acuña <aaronacu_85@hotmail.com>
2024-08-06 18:54:01 -0500

    feat(interaction): implemented command and query service for chat.

---
+++ HelpTechService/Interaction/Application/Internal/CommandServices/ChatCommandService.cs
@@ -0,0 +1,28 @@
+﻿using HelpTechService.Interaction.Domain.Model.Commands.Chat;
+using HelpTechService.Interaction.Domain.Repositories;
+using HelpTechService.Interaction.Domain.Services.Chat;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Interaction.Application.Internal.CommandServices
+{
+    internal class ChatCommandService
+        (IChatRepository chatRepository,
+        IUnitOfWork unitOfWork) :
+        IChatCommandService
+    {
+        public async Task<bool> Handle
+            (SendMessageCommand command)
+        {
+            try
+            {
+                await chatRepository
+                    .AddAsync(new(command));
+
+                await unitOfWork.CompleteAsync();
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
---
+++ HelpTechService/Interaction/Application/Internal/QueryServices/ChatQueryService.cs
@@ -0,0 +1,18 @@
+﻿using HelpTechService.Interaction.Domain.Model.Aggregates;
+using HelpTechService.Interaction.Domain.Model.Queries.Chat;
+using HelpTechService.Interaction.Domain.Repositories;
+using HelpTechService.Interaction.Domain.Services.Chat;
+
+namespace HelpTechService.Interaction.Application.Internal.QueryServices
+{
+    internal class ChatQueryService
+        (IChatRepository chatRepository) :
+        IChatQueryService
+    {
+        public async Task<IEnumerable<Chat>> Handle
+            (GetChatByChatRoomIdQuery query) =>
+            await chatRepository
+            .FindByChatRoomIdAsync
+            (query.ChatRoomId);
+    }
+}
