995a1554a5128fdc800073357867c1a3d04c9e56
aaron acuña <aaronacu_85@hotmail.com>
2024-08-06 18:47:16 -0500

    feat(interaction): added command and query for chat member.

---
+++ HelpTechService/Interaction/Domain/Services/ChatMember/IChatMemberQueryService.cs
@@ -0,0 +1,13 @@
+﻿using HelpTechService.Interaction.Domain.Model.Queries.ChatMember;
+
+namespace HelpTechService.Interaction.Domain.Services.ChatMember
+{
+    public interface IChatMemberQueryService
+    {
+        Task<IEnumerable<Model.Aggregates.ChatMember>> Handle
+            (GetChatMembersByTechnicalIdQuery query);
+
+        Task<IEnumerable<Model.Aggregates.ChatMember>> Handle
+            (GetChatMembersByConsumerIdQuery query);
+    }
+}
