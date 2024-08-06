62cd78f00c55a3d3e913e563e47f87468efaa69a
aaron acuña <aaronacu_85@hotmail.com>
2024-08-05 19:05:16 -0500

    feat(report): implemented repository for type complaint.

---
+++ HelpTechService/Report/Infrastructure/Persistence/EFC/Repositories/TypeComplaintRepository.cs
@@ -0,0 +1,13 @@
+﻿using HelpTechService.Report.Domain.Model.Entities;
+using HelpTechService.Report.Domain.Repositories;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace HelpTechService.Report.Infrastructure.Persistence.EFC.Repositories
+{
+    internal class TypeComplaintRepository
+        (HelpTechContext context) :
+        BaseRepository<TypeComplaint>(context),
+        ITypeComplaintRepository
+    { }
+}
