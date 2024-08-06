82f8265929c747edebcc04b903021c31348f223c
aaron acuña <aaronacu_85@hotmail.com>
2024-08-06 15:10:04 -0500

    feat(statistic): implemented query service.

---
+++ HelpTechService/Statistic/Application/Internal/QueryServices/StatisticQueryService.cs
@@ -0,0 +1,29 @@
+﻿using HelpTechService.Statistic.Domain.Model.Queries;
+using HelpTechService.Statistic.Domain.Repositories;
+using HelpTechService.Statistic.Domain.Services;
+
+namespace HelpTechService.Statistic.Application.Internal.QueryServices
+{
+    internal class StatisticQueryService
+        (IStatisticRepository statisticRepository) :
+        IStatisticQueryService
+    {
+        public async Task<dynamic?> Handle
+            (GetGeneralTechnicalStatisticQuery query) =>
+            await statisticRepository
+            .GeneralTechnicalStatisticAsync
+            (query.TechnicalId);
+
+        public async Task<dynamic?> Handle
+            (GetDetailedTechnicalStatisticQuery query) =>
+            await statisticRepository
+            .DetailedTechnicalStatisticAsync
+            (query.TechnicalId, query.TypeStatistic);
+
+        public async Task<dynamic?> Handle
+            (GetReviewStatisticQuery query) =>
+            await statisticRepository
+            .ReviewStatisticAsync
+            (query.TechnicalId);
+    }
+}
