3dcdaa1672357d2aa0298741933300665c3014fc
aaron acuña <aaronacu_85@hotmail.com>
2024-08-06 18:39:22 -0500

    feat(interaction): added repository for chat.

---
+++ HelpTechService/Interaction/Domain/Repositories/IChatRepository.cs
@@ -0,0 +1,12 @@
+﻿using HelpTechService.Interaction.Domain.Model.Aggregates;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Interaction.Domain.Repositories
+{
+    public interface IChatRepository :
+        IBaseRepository<Chat>
+    {
+        Task<IEnumerable<Chat>> FindByChatRoomIdAsync
+            (int chatRoomId);
+    }
+}
