6f07174b1127f9509fc33e8fd4797960e89338d3
aaron acuña <aaronacu_85@hotmail.com>
2024-08-06 18:41:31 -0500

    feat(interaction): added repository for chat member.

---
+++ HelpTechService/Interaction/Domain/Repositories/IChatMemberRepository.cs
@@ -0,0 +1,15 @@
+﻿using HelpTechService.Interaction.Domain.Model.Aggregates;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Interaction.Domain.Repositories
+{
+    public interface IChatMemberRepository :
+        IBaseRepository<ChatMember>
+    {
+        Task<IEnumerable<ChatMember>> FindByTechnicalIdAsync
+            (int technicalId);
+
+        Task<IEnumerable<ChatMember>> FindByConsumerIdAsync
+            (int consumerId);
+    }
+}
