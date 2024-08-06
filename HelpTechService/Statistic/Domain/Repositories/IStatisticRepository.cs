a513161b94025c3a4f746f6d6d9f36d39303da35
aaron acuña <aaronacu_85@hotmail.com>
2024-08-06 14:59:23 -0500

    feat(statistic): added respository.

---
+++ HelpTechService/Statistic/Domain/Repositories/IStatisticRepository.cs
@@ -0,0 +1,16 @@
+﻿using HelpTechService.Statistic.Domain.Model.ValueObjects;
+
+namespace HelpTechService.Statistic.Domain.Repositories
+{
+    public interface IStatisticRepository
+    {
+        Task<dynamic?> DetailedTechnicalStatisticAsync
+            (int technicalId, ETypeStatistic typeStatistic);
+
+        Task<dynamic?> GeneralTechnicalStatisticAsync
+            (int technicalId);
+
+        Task<dynamic?> ReviewStatisticAsync
+            (int technicalId);
+    }
+}
