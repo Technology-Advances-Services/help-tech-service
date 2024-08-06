abe86f1f6a6514b5dd9cfc3d2d6dfa2eda94b9da
aaron acuña <aaronacu_85@hotmail.com>
2024-08-06 18:37:36 -0500

    feat(interaction): added queries for chat and chat member.

---
+++ HelpTechService/Interaction/Domain/Model/Queries/Chat/GetChatByChatRoomIdQuery.cs
@@ -0,0 +1,5 @@
+﻿namespace HelpTechService.Interaction.Domain.Model.Queries.Chat
+{
+    public record GetChatByChatRoomIdQuery
+        (int ChatRoomId);
+}
---
+++ HelpTechService/Interaction/Domain/Model/Queries/ChatMember/GetChatMembersByTechnicalIdQuery.cs
@@ -0,0 +1,5 @@
+﻿namespace HelpTechService.Interaction.Domain.Model.Queries.ChatMember
+{
+    public record GetChatMembersByTechnicalIdQuery
+        (int TechnicalId);
+}
