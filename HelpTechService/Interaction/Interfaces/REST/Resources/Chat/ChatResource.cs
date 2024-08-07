e2627331c3fdaae08d5907946a6ad2e8961e00ea
aaron acuña <aaronacu_85@hotmail.com>
2024-08-07 16:01:46 -0500

    feat(interaction): added resources for chat.

---
+++ HelpTechService/Interaction/Interfaces/REST/Resources/Chat/ChatResource.cs
@@ -0,0 +1,7 @@
+﻿namespace HelpTechService.Interaction.Interfaces.REST.Resources.Chat
+{
+    public record ChatResource
+        (int ChatRoomId, int? TechnicalId,
+        int? ConsumerId, DateTime ShippingDate,
+        string Message);
+}
---
+++ HelpTechService/Interaction/Interfaces/REST/Resources/Chat/SendMessageResource.cs
@@ -0,0 +1,6 @@
+﻿namespace HelpTechService.Interaction.Interfaces.REST.Resources.Chat
+{
+    public record SendMessageResource
+        (int ChatRoomId, int? TechnicalId,
+        int? ConsumerId, string Message);
+}
