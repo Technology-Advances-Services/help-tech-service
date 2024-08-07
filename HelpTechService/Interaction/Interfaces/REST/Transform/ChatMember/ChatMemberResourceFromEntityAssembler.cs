9228ff8ede79d1bdf69d07ce4f39cf63577f0deb
aaron acuña <aaronacu_85@hotmail.com>
2024-08-07 16:10:40 -0500

    feat(interaction): added transform for chat member.

---
+++ HelpTechService/Interaction/Interfaces/REST/Transform/ChatMember/ChatMemberResourceFromEntityAssembler.cs
@@ -0,0 +1,12 @@
+﻿using HelpTechService.Interaction.Interfaces.REST.Resources.ChatMember;
+
+namespace HelpTechService.Interaction.Interfaces.REST.Transform.ChatMember
+{
+    public class ChatMemberResourceFromEntityAssembler
+    {
+        public static ChatMemberResource ToResourceFromEntity
+            (Domain.Model.Aggregates.ChatMember entity) =>
+            new(entity.ChatsRoomsId, entity.TechnicalsId,
+                entity.ConsumersId);
+    }
+}
