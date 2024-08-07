5fd19c6c9d8f6d833014585231806f16df48cb27
aaron acuña <aaronacu_85@hotmail.com>
2024-08-07 16:39:51 -0500

    feat(interaction): added controller for chat.

---
+++ HelpTechService/Interaction/Interfaces/REST/ChatsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using HelpTechService.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using HelpTechService.Interaction.Domain.Services.Chat;
+using HelpTechService.Interaction.Interfaces.REST.Resources.Chat;
+using HelpTechService.Interaction.Interfaces.REST.Transform.Chat;
+
+namespace HelpTechService.Interaction.Interfaces.REST
+{
+    [Route("api/chats/")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Authorize("TECNICO", "CONSUMIDOR")]
+    public class ChatsController
+        (IChatCommandService chatCommandService,
+        IChatQueryService chatQueryService) :
+        ControllerBase
+    {
+        [Route("send-message")]
+        [HttpPost]
+        public async Task<IActionResult> SendMessage
+            ([FromBody] SendMessageResource resource)
+        {
+            var chat = await chatCommandService
+                .Handle(SendMessageCommandFromResourceAssembler
+                .ToCommandFromResource(resource));
+
+            return Ok(chat);
+        }
+
+        [Route("chats-by-chat-room")]
+        [HttpGet]
+        public async Task<IActionResult> GetChatByChatRoomId
+            ([FromQuery] int chatRoomId)
+        {
+            var chats = await chatQueryService
+                .Handle(new Domain.Model.Queries.Chat.GetChatByChatRoomIdQuery(chatRoomId));
+
+            var chatsResource = chats.Select
+                (ChatResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(chatsResource);
+        }
+    }
+}
