144f8287b0d4a3cea63a27bf6cae9f439f767a71
aaron acuña <aaronacu_85@hotmail.com>
2024-08-07 16:25:50 -0500

    feat(interaction): added transform for chat.

---
+++ HelpTechService/Interaction/Interfaces/REST/Transform/Chat/ChatResourceFromEntityAssembler.cs
@@ -0,0 +1,13 @@
+﻿using HelpTechService.Interaction.Interfaces.REST.Resources.Chat;
+
+namespace HelpTechService.Interaction.Interfaces.REST.Transform.Chat
+{
+    public class ChatResourceFromEntityAssembler
+    {
+        public static ChatResource ToResourceFromEntity
+            (Domain.Model.Aggregates.Chat entity) =>
+            new(entity.ChatsRoomsId, entity.TechnicalsId,
+                entity.ConsumersId, entity.ShippingDate,
+                entity.Message);
+    }
+}
---
+++ HelpTechService/Interaction/Interfaces/REST/Transform/Chat/SendMessageCommandFromResourceAssembler.cs
@@ -0,0 +1,24 @@
+﻿using HelpTechService.Interaction.Domain.Model.Commands.Chat;
+using HelpTechService.Interaction.Interfaces.REST.Resources.Chat;
+
+namespace HelpTechService.Interaction.Interfaces.REST.Transform.Chat
+{
+    public class SendMessageCommandFromResourceAssembler
+    {
+        public static SendMessageCommand ToCommandFromResource
+            (SendMessageResource resource)
+        {
+            if (resource.Sender == "TECNICO")
+                return new(resource.ChatRoomId,
+                    resource.PersonId, null,
+                    resource.Message);
+
+            else if (resource.Sender == "CONSUMIDOR")
+                return new(resource.ChatRoomId, null,
+                    resource.PersonId, resource.Message);
+
+            else
+                throw new Exception("The sender does not exist!");
+        }
+    }
+}
