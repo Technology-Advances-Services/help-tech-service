d51295e4ddd965505ebfdbf711e0cbe342f82954
aaron acuña <aaronacu_85@hotmail.com>
2024-08-07 15:54:39 -0500

    feat(interaction): implemented chat member repository.

---
+++ HelpTechService/Interaction/Infrastructure/Persistence/EFC/Repositories/ChatMemberRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HelpTechService.Interaction.Domain.Model.Aggregates;
+using HelpTechService.Interaction.Domain.Repositories;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace HelpTechService.Interaction.Infrastructure.Persistence.EFC.Repositories
+{
+    internal class ChatMemberRepository
+        (HelpTechContext context) :
+        BaseRepository<ChatMember>(context),
+        IChatMemberRepository
+    {
+        public async Task<IEnumerable<ChatMember>> FindByTechnicalIdAsync
+            (int technicalId) => await Context.Set<ChatMember>()
+            .Where(c => c.TechnicalsId == technicalId)
+            .ToListAsync();
+
+        public async Task<IEnumerable<ChatMember>> FindByConsumerIdAsync
+            (int consumerId) => await Context.Set<ChatMember>()
+            .Where(c => c.ConsumersId == consumerId)
+            .ToListAsync();
+    }
+}
