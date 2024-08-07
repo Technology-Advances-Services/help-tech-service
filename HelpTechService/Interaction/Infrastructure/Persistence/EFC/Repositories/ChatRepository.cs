ef15317e19cecc28391e367f932a25717c2682a2
aaron acuña <aaronacu_85@hotmail.com>
2024-08-07 15:46:54 -0500

    feat(interaction): implemented chat repository.

---
+++ HelpTechService/Interaction/Infrastructure/Persistence/EFC/Repositories/ChatRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HelpTechService.Interaction.Domain.Model.Aggregates;
+using HelpTechService.Interaction.Domain.Repositories;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace HelpTechService.Interaction.Infrastructure.Persistence.EFC.Repositories
+{
+    internal class ChatRepository
+        (HelpTechContext context) :
+        BaseRepository<Chat>(context),
+        IChatRepository
+    {
+        public async Task<IEnumerable<Chat>> FindByChatRoomIdAsync
+            (int chatRoomId) => await Context.Set<Chat>()
+            .Where(c => c.ChatsRoomsId == chatRoomId)
+            .ToListAsync();
+    }
+}
