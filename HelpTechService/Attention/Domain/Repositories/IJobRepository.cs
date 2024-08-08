c304f9e4b20c9914f26eddf179b63c694c370557
aaron acuña <aaronacu_85@hotmail.com>
2024-08-08 18:13:35 -0500

    feat(atttention): added repository for job.

---
+++ HelpTechService/Attention/Domain/Repositories/IJobRepository.cs
@@ -0,0 +1,22 @@
+﻿using HelpTechService.Attention.Domain.Model.Aggregates;
+using HelpTechService.Attention.Domain.Model.ValueObjects.Job;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Attention.Domain.Repositories
+{
+    public interface IJobRepository :
+        IBaseRepository<Job>
+    {
+        Task<IEnumerable<Job>> FindByTechnicalIdAsync
+            (int technicalId);
+
+        Task<IEnumerable<Job>> FindByConsumerIdAsync
+            (int consumerId);
+
+        Task<IEnumerable<Job>> FindByTechnicalIdAndStateAsync
+            (int technicalId, EJobState jobState);
+
+        Task<IEnumerable<Job>> FindByConsumerIdAndStateAsync
+            (int consumerId, EJobState jobState);
+    }
+}
