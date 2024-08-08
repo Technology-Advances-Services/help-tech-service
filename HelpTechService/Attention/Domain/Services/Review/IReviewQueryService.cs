8045c7b0d14a35f971ab805ead63deb097430453
aaron acuña <aaronacu_85@hotmail.com>
2024-08-08 18:08:23 -0500

    feat(attention): added query service for review.

---
+++ HelpTechService/Attention/Domain/Services/Review/IReviewQueryService.cs
@@ -0,0 +1,10 @@
+﻿using HelpTechService.Attention.Domain.Model.Queries.Review;
+
+namespace HelpTechService.Attention.Domain.Services.Review
+{
+    public interface IReviewQueryService
+    {
+        Task<IEnumerable<Model.Entities.Review>> Handle
+            (GetReviewsByTechnicalIdQuery query);
+    }
+}
