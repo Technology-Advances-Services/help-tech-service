0790650f915530b9a1dbde1371a8c7ed58d420d4
aaron acuña <aaronacu_85@hotmail.com>
2024-08-08 18:10:58 -0500

    feat(attention): added repository for review.

---
+++ HelpTechService/Attention/Domain/Repositories/IReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using HelpTechService.Attention.Domain.Model.Entities;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Attention.Domain.Repositories
+{
+    public interface IReviewRepository :
+        IBaseRepository<Review>
+    {
+        Task<IEnumerable<Review>> FindByTechnicalIdAsync
+            (int technicalId);
+    }
+}
