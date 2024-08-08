6b2fe335c4c4454d9705a47664b1b02812b36e08
aaron acuña <aaronacu_85@hotmail.com>
2024-08-08 17:56:45 -0500

    feat(attention): added command for review.

---
+++ HelpTechService/Attention/Domain/Model/Commands/Review/AddReviewToJobCommand.cs
@@ -0,0 +1,9 @@
+﻿using HelpTechService.Attention.Domain.Model.ValueObjects.Review;
+
+namespace HelpTechService.Attention.Domain.Model.Commands.Review
+{
+    public record AddReviewToJobCommand
+        (int TechnicalId, int ConsumerId,
+        int Score, string Opinion,
+        EReviewState ReviewState);
+}
--- HelpTechService/Attention/Domain/Model/Entities/Review.cs
+++ HelpTechService/Attention/Domain/Model/Entities/Review.cs
@@ -1,4 +1,6 @@
-﻿using HelpTechService.IAM.Domain.Model.Aggregates;
+﻿using HelpTechService.Attention.Domain.Model.Commands.Review;
+using HelpTechService.Attention.Domain.Model.ValueObjects.Review;
+using HelpTechService.IAM.Domain.Model.Aggregates;
 
 namespace HelpTechService.Attention.Domain.Model.Entities
 {
@@ -14,5 +16,37 @@ namespace HelpTechService.Attention.Domain.Model.Entities
 
         public virtual Consumer Consumer { get; } = null!;
         public virtual Technical Technical { get; } = null!;
+
+        public Review()
+        {
+            this.TechnicalsId = 0;
+            this.ConsumersId = 0;
+            this.Score = 0;
+            this.Opinion = string.Empty;
+            this.State = string.Empty;
+        }
+        public Review
+            (int technicalId, int consumerId,
+            int score, string opinion,
+            EReviewState reviewState)
+        {
+            this.TechnicalsId = technicalId;
+            this.ConsumersId = consumerId;
+            this.ShippingDate = DateTime.Now;
+            this.Score = score;
+            this.Opinion = opinion;
+            this.State = reviewState.ToString();
+        }
+        public Review
+            (AddReviewToJobCommand command)
+        {
+            this.TechnicalsId = command.TechnicalId;
+            this.ConsumersId = command.ConsumerId;
+            this.ShippingDate = DateTime.Now;
+            this.Score = command.Score;
+            this.Opinion = command.Opinion;
+            this.State = command.ReviewState
+                .ToString();
+        }
     }
 }
