0525e139a9266c1059cd0033e95cacae8537d882
aaron acuña <aaronacu_85@hotmail.com>
2024-08-08 18:06:14 -0500

    feat(attention): added query service for job.

---
+++ HelpTechService/Attention/Domain/Services/Job/IJobQueryService.cs
@@ -0,0 +1,22 @@
+﻿using HelpTechService.Attention.Domain.Model.Queries.Job;
+
+namespace HelpTechService.Attention.Domain.Services.Job
+{
+    public interface IJobQueryService
+    {
+        Task<Model.Aggregates.Job?> Handle
+            (GetJobByIdQuery query);
+
+        Task<IEnumerable<Model.Aggregates.Job>> Handle
+            (GetJobsByTechnicalIdQuery query);
+
+        Task<IEnumerable<Model.Aggregates.Job>> Handle
+            (GetJobsByConsumerIdQuery query);
+
+        Task<IEnumerable<Model.Aggregates.Job>> Handle
+            (GetJobsByTechnicalIdAndStateQuery query);
+
+        Task<IEnumerable<Model.Aggregates.Job>> Handle
+            (GetJobsByConsumerIdAndStateQuery query);
+    }
+}
