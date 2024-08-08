10a92f0ba75c698d7e4ad9b58f0bb25cca87bd1a
aaron acuña <aaronacu_85@hotmail.com>
2024-08-08 18:07:07 -0500

    feat(attention): added command service for review.

---
+++ HelpTechService/Attention/Domain/Services/Review/IReviewCommandService.cs
@@ -0,0 +1,10 @@
+﻿using HelpTechService.Attention.Domain.Model.Commands.Review;
+
+namespace HelpTechService.Attention.Domain.Services.Review
+{
+    public interface IReviewCommandService
+    {
+        Task<bool> Handle
+            (AddReviewToJobCommand command);
+    }
+}
