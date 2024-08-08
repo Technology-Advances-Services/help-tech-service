850b4a1f66791383bc2b2257adaa4e3af222edd4
aaron acuña <aaronacu_85@hotmail.com>
2024-08-08 18:04:20 -0500

    feat(attention): added command service for job.

---
+++ HelpTechService/Attention/Domain/Services/Job/IJobCommandService.cs
@@ -0,0 +1,16 @@
+﻿using HelpTechService.Attention.Domain.Model.Commands.Job;
+
+namespace HelpTechService.Attention.Domain.Services.Job
+{
+    public interface IJobCommandService
+    {
+        Task<bool> Handle
+            (RegisterRequestJobCommand command);
+
+        Task<bool> Handle
+            (AssignJobDetailCommand command);
+
+        Task<bool> Handle
+            (UpdateJobStateCommand command)
+    }
+}
