e627baa5e067031170dfb35107a64ce4d34e71b0
aaron acuña <aaronacu_85@hotmail.com>
2024-08-09 17:27:26 -0500

    feat(attention): added resource for review.

---
+++ HelpTechService/Attention/Interfaces/REST/Resources/Review/AddReviewToJobResource.cs
@@ -0,0 +1,7 @@
+﻿namespace HelpTechService.Attention.Interfaces.REST.Resources.Review
+{
+    public record AddReviewToJobResource
+        (int TechnicalId, int ConsumerId,
+        int Score, string Opinion,
+        string ReviewState);
+}
---
+++ HelpTechService/Attention/Interfaces/REST/Resources/Review/ReviewResource.cs
@@ -0,0 +1,7 @@
+﻿namespace HelpTechService.Attention.Interfaces.REST.Resources.Review
+{
+    public record ReviewResource
+        (int TechnicalId, int ConsumerId,
+        int Score, string Opinion,
+        string ReviewState);
+}
