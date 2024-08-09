59a5a449a3bd18bc1a098193db861d0a8f7fe27d
aaron acuña <aaronacu_85@hotmail.com>
2024-08-09 16:17:29 -0500

    feat(attention): implemented query service for review.

---
+++ HelpTechService/Attention/Application/Internal/QueryServices/ReviewQueryService.cs
@@ -0,0 +1,18 @@
+﻿using HelpTechService.Attention.Domain.Model.Entities;
+using HelpTechService.Attention.Domain.Model.Queries.Review;
+using HelpTechService.Attention.Domain.Repositories;
+using HelpTechService.Attention.Domain.Services.Review;
+
+namespace HelpTechService.Attention.Application.Internal.QueryServices
+{
+    public class ReviewQueryService
+        (IReviewRepository reviewRepository) :
+        IReviewQueryService
+    {
+        public async Task<IEnumerable<Review>> Handle
+            (GetReviewsByTechnicalIdQuery query) =>
+            await reviewRepository
+            .FindByTechnicalIdAsync
+            (query.TechnicalId);
+    }
+}
