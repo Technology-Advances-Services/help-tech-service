548d5e12e51910925e8c814052ef4384b380672c
aaron acuña <aaronacu_85@hotmail.com>
2024-08-09 16:25:28 -0500

    feat(attention): implemented query service for job.

---
+++ HelpTechService/Attention/Application/Internal/QueryServices/JobQueryService.cs
@@ -0,0 +1,41 @@
+﻿using HelpTechService.Attention.Domain.Model.Aggregates;
+using HelpTechService.Attention.Domain.Model.Queries.Job;
+using HelpTechService.Attention.Domain.Repositories;
+using HelpTechService.Attention.Domain.Services.Job;
+
+namespace HelpTechService.Attention.Application.Internal.QueryServices
+{
+    public class JobQueryService
+        (IJobRepository jobRepository) :
+        IJobQueryService
+    {
+        public Task<Job?> Handle
+            (GetJobByIdQuery query) =>
+            jobRepository.FindByIdAsync
+            (query.Id);
+
+        public async Task<IEnumerable<Job>> Handle
+            (GetJobsByTechnicalIdQuery query) =>
+            await jobRepository
+            .FindByTechnicalIdAsync
+            (query.TechnicalId);
+
+        public async Task<IEnumerable<Job>> Handle
+            (GetJobsByConsumerIdQuery query) =>
+            await jobRepository
+            .FindByConsumerIdAsync
+            (query.ConsumerId);
+
+        public async Task<IEnumerable<Job>> Handle
+            (GetJobsByTechnicalIdAndStateQuery query) =>
+            await jobRepository
+            .FindByTechnicalIdAndStateAsync
+            (query.TechnicalId, query.JobState);
+
+        public async Task<IEnumerable<Job>> Handle
+            (GetJobsByConsumerIdAndStateQuery query) =>
+            await jobRepository
+            .FindByConsumerIdAndStateAsync
+            (query.ConsumerId, query.JobState);
+    }
+}
