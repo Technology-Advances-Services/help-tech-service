46ab6e14e656fd11aea9e64635436f1f84f95f28
aaron acuña <aaronacu_85@hotmail.com>
2024-08-09 18:22:55 -0500

    feat(attention): added controller for review.

---
+++ HelpTechService/Attention/Interfaces/REST/ReviewsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using HelpTechService.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using HelpTechService.Attention.Domain.Model.Queries.Review;
+using HelpTechService.Attention.Domain.Services.Review;
+using HelpTechService.Attention.Interfaces.REST.Resources.Review;
+using HelpTechService.Attention.Interfaces.REST.Transform.Review;
+
+namespace HelpTechService.Attention.Interfaces.REST
+{
+    [Route("api/reviews/")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Authorize]
+    public class ReviewsController
+        (IReviewCommandService reviewCommandService,
+        IReviewQueryService reviewQueryService) :
+        ControllerBase
+    {
+        [Route("add-review-to-job")]
+        [HttpPost]
+        [Authorize("CONSUMIDOR")]
+        public async Task<IActionResult> AddReviewToJob
+            ([FromBody] AddReviewToJobResource resource)
+        {
+            var result = await reviewCommandService
+                .Handle(AddReviewToJobCommandFromResourceAssembler
+                .ToCommandFromResource(resource));
+
+            return Ok(result);
+        }
+
+        [Route("reviews-by-technical")]
+        [HttpGet]
+        [Authorize("TECNICO", "CONSUMIDOR")]
+        public async Task<IActionResult> GetReviewsByTechnicalId
+            ([FromQuery] int technicalId)
+        {
+            var reviews = await reviewQueryService
+                .Handle(new GetReviewsByTechnicalIdQuery
+                (technicalId));
+
+            var reviewsResource = reviews.Select
+                (ReviewResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(reviewsResource);
+        }
+    }
+}
