5833fa8e0434e869542c60a0b2e2f1fa91579a8e
aaron acuña <aaronacu_85@hotmail.com>
2024-08-09 15:58:36 -0500

    feat(attention): implemented command service for review.

---
+++ HelpTechService/Attention/Application/Internal/CommandServices/ReviewCommandService.cs
@@ -0,0 +1,28 @@
+﻿using HelpTechService.Attention.Domain.Model.Commands.Review;
+using HelpTechService.Attention.Domain.Repositories;
+using HelpTechService.Attention.Domain.Services.Review;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Attention.Application.Internal.CommandServices
+{
+    internal class ReviewCommandService
+        (IReviewRepository reviewRepository,
+        IUnitOfWork unitOfWork) :
+        IReviewCommandService
+    {
+        public async Task<bool> Handle
+            (AddReviewToJobCommand command)
+        {
+            try
+            {
+                await reviewRepository
+                    .AddAsync(new(command));
+
+                await unitOfWork.CompleteAsync();
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
