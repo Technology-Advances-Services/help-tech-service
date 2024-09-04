dd0cacacff3c232de87fa8db86d857a29f5698a8
aaron acuña <aaronacu_85@hotmail.com>
2024-09-04 18:17:52 -0500

    feat(iam): added transform for update technical credential.

---
+++ HelpTechService/IAM/Interfaces/REST/Transform/TechnicalCredential/UpdateTechnicalCredentialCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using HelpTechService.IAM.Domain.Model.Commands.TechnicalCredential;
+using HelpTechService.IAM.Interfaces.REST.Resources.User;
+
+namespace HelpTechService.IAM.Interfaces.REST.Transform.TechnicalCredential
+{
+    public class UpdateTechnicalCredentialCommandFromResourceAssembler
+    {
+        public static UpdateTechnicalCredentialCommand ToCommandFromResource
+            (UpdateCredentialResource resource) =>
+            new(resource.Username, resource.Code);
+    }
+}
