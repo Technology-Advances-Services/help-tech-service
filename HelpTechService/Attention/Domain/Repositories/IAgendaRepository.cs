528bd2138e75a2c04a4df8c5d5dd1da46be564c2
aaron acuña <aaronacu_85@hotmail.com>
2024-09-16 21:03:53 -0500

    feat(attention): added repository for agenda.

---
+++ HelpTechService/Attention/Domain/Repositories/IAgendaRepository.cs
@@ -0,0 +1,12 @@
+﻿using HelpTechService.Attention.Domain.Model.Entities;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Attention.Domain.Repositories
+{
+    public interface IAgendaRepository :
+        IBaseRepository<Agenda>
+    {
+        Task<Agenda?> FindByTechnicalIdAsync
+            (int technicalId);
+    }
+}
