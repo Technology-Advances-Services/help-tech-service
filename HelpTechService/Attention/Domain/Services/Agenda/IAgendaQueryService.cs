6fced1ca279200336384e0157f6073246a2f5d14
aaron acuña <aaronacu_85@hotmail.com>
2024-09-16 21:04:16 -0500

    feat(attention): added service for agenda.

---
+++ HelpTechService/Attention/Domain/Services/Agenda/IAgendaQueryService.cs
@@ -0,0 +1,10 @@
+﻿using HelpTechService.Attention.Domain.Model.Queries.Agenda;
+
+namespace HelpTechService.Attention.Domain.Services.Agenda
+{
+    public interface IAgendaQueryService
+    {
+        Task<Model.Entities.Agenda?> Handle
+            (GetAgendaByTechnicalIdQuery query);
+    }
+}
