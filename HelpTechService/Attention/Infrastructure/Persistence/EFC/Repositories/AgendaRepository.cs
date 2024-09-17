fb284aaee1d7b5bbd739002379f85eb3065a3da9
aaron acuña <aaronacu_85@hotmail.com>
2024-09-16 21:04:55 -0500

    feat(attention): implemented repository for agenda.

---
+++ HelpTechService/Attention/Infrastructure/Persistence/EFC/Repositories/AgendaRepository.cs
@@ -0,0 +1,19 @@
+﻿using HelpTechService.Attention.Domain.Model.Entities;
+using HelpTechService.Attention.Domain.Repositories;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpTechService.Attention.Infrastructure.Persistence.EFC.Repositories
+{
+    public class AgendaRepository
+        (HelpTechContext context) :
+        BaseRepository<Agenda>(context),
+        IAgendaRepository
+    {
+        public async Task<Agenda?> FindByTechnicalIdAsync
+            (int technicalId) => await Context.Set<Agenda>()
+            .Where(a => a.TechnicalsId == technicalId)
+            .FirstOrDefaultAsync();
+    }
+}
