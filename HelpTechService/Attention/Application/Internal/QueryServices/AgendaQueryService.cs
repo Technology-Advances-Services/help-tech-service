c008e97fda0cb066277b37315de7bf5d2fc8419c
aaron acuña <aaronacu_85@hotmail.com>
2024-09-16 21:04:31 -0500

    feat(attention): implemente service for agenda.

---
+++ HelpTechService/Attention/Application/Internal/QueryServices/AgendaQueryService.cs
@@ -0,0 +1,19 @@
+﻿using HelpTechService.Attention.Domain.Model.Entities;
+using HelpTechService.Attention.Domain.Model.Queries.Agenda;
+using HelpTechService.Attention.Domain.Repositories;
+using HelpTechService.Attention.Domain.Services.Agenda;
+
+namespace HelpTechService.Attention.Application.Internal.QueryServices
+{
+    public class AgendaQueryService
+        (IAgendaRepository agendaRepository) :
+        IAgendaQueryService
+    {
+        public async Task<Agenda?> Handle
+            (GetAgendaByTechnicalIdQuery query) =>
+            await agendaRepository
+            .FindByTechnicalIdAsync
+            (int.Parse(query.TechnicalId
+                .TrimStart('0')));
+    }
+}
