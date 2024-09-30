7978fd083b77919946653ba402accbedde23751f
aaron acuña <aaronacu_85@hotmail.com>
2024-09-30 16:54:04 -0500

    fix(iam): correted queries in consumer credential repository.

--- HelpTechService/IAM/Infrastructure/Persistence/EFC/Repositories/ConsumerCredentialRepository.cs
+++ HelpTechService/IAM/Infrastructure/Persistence/EFC/Repositories/ConsumerCredentialRepository.cs
@@ -20,7 +20,8 @@ namespace HelpTechService.IAM.Infrastructure.Persistence.EFC.Repositories
                  join co in Context.Set<Consumer>()
                  on cc.ConsumersId equals consumerId
                  where co.State == "ACTIVO"
-                 select cc).FirstOrDefaultAsync();
+                 select cc).AsNoTracking()
+                 .FirstOrDefaultAsync();
 
             return result?.Code;
         }
