1217c948690d2af99caf4dda7ad8757f91930e5e
aaron acuña <aaronacu_85@hotmail.com>
2024-09-30 16:53:29 -0500

    fix(iam): corrected validation credentials in middleware.

--- HelpTechService/IAM/Infrastructure/Pipeline/Middleware/Components/RequestAuthorizationMiddleware.cs
+++ HelpTechService/IAM/Infrastructure/Pipeline/Middleware/Components/RequestAuthorizationMiddleware.cs
@@ -1,9 +1,5 @@
 ﻿using HelpTechService.IAM.Application.Internal.OutboundServices;
-using HelpTechService.IAM.Domain.Model.Queries.ConsumerCredential;
-using HelpTechService.IAM.Domain.Model.Queries.TechnicalCredential;
 using HelpTechService.IAM.Domain.Model.ValueObjects.Credential;
-using HelpTechService.IAM.Domain.Services.ConsumerCredential;
-using HelpTechService.IAM.Domain.Services.TechnicalCredential;
 using HelpTechService.IAM.Infrastructure.Pipeline.Middleware.Attributes;
 
 namespace HelpTechService.IAM.Infrastructure.Pipeline.Middleware.Components
@@ -13,8 +9,6 @@ namespace HelpTechService.IAM.Infrastructure.Pipeline.Middleware.Components
     {
         public async Task InvokeAsync
             (HttpContext context,
-            ITechnicalCredentialQueryService technicalCredentialQueryService,
-            IConsumerCredentialQueryService consumerCredentialQueryService,
             ITokenService tokenService)
         {
             var allowAnonymous =
@@ -36,27 +30,14 @@ namespace HelpTechService.IAM.Infrastructure.Pipeline.Middleware.Components
             var tokenResult = tokenService.ValidateToken(token) ??
                 throw new Exception("Invalid token!");
 
-            dynamic? validation = null;
+            var validation =
+                tokenResult.Role == ECredentialRole.TECNICO.ToString() ||
+                tokenResult.Role == ECredentialRole.CONSUMIDOR.ToString();
 
-            if (tokenResult.Role == ECredentialRole
-                .TECNICO.ToString())
-                validation = await technicalCredentialQueryService.Handle
-                    (new GetTechnicalCredentialByTechnicalIdAndCodeQuery
-                    (tokenResult.Id, tokenResult.Code));
-
-            else if (tokenResult.Role == ECredentialRole
-                .CONSUMIDOR.ToString())
-                validation = await consumerCredentialQueryService.Handle
-                    (new GetConsumerCredentialByConsumerIdAndCodeQuery
-                    (tokenResult.Id, tokenResult.Code));
-
-            if (validation is null)
+            if (validation is false)
                 throw new Exception("Invalid credentials!");
 
-            if (validation.Result is true)
-                context.Items["Credentials"] = tokenResult;
-
-            else throw new Exception("Credentials not found!");
+            context.Items["Credentials"] = tokenResult;
 
             await next(context);
         }
