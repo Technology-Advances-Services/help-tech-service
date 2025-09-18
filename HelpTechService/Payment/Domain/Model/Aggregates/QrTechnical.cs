333ac44364cf2d5f9a439cd38ea2a911d43159dc
aaron acuña <aaronacu_85@hotmail.com>
2025-09-18 14:15:15 -0500

    fix: corrected technicalId property.

--- HelpTechService/Payment/Domain/Model/Aggregates/QrTechnical.cs
+++ HelpTechService/Payment/Domain/Model/Aggregates/QrTechnical.cs
@@ -1,15 +1,34 @@
-﻿using HelpTechService.IAM.Domain.Model.Aggregates;
+﻿using TimeZoneConverter;
+using HelpTechService.IAM.Domain.Model.Aggregates;
+using HelpTechService.Payment.Domain.Model.ValueObjects;
 
 namespace HelpTechService.Payment.Domain.Model.Aggregates
 {
     public class QrTechnical
     {
         public int Id { get; set; }
-        public int TechnicalsId { get; set; }
+        public int TechnicalId { get; set; }
         public DateTime RegistrationDate { get; set; }
         public string QrUrl { get; set; } = null!;
         public string State { get; set; } = null!;
 
         public virtual Technical Technical { get; } = null!;
+
+        public QrTechnical()
+        {
+            this.Id = 0;
+            this.TechnicalId = 0;
+            this.QrUrl = string.Empty;
+            this.State = string.Empty;
+        }
+
+        public static DateTime GetLimaNow()
+        {
+            DateTime utcNow = DateTime.UtcNow;
+
+            var limaZone = TZConvert.GetTimeZoneInfo("America/Lima");
+
+            return TimeZoneInfo.ConvertTimeFromUtc(utcNow, limaZone);
+        }
     }
 }
