d26139740ea1bb410b6328be2edc87e74d7b6bbc
Mauricio Rojas <mauriciorojasanchez@gmail.com>
2025-09-24 15:57:24 -0500

    feat(payment): added interfaces.

---
+++ HelpTechService/Payment/Interfaces/REST/Resources/QrTechnical/QrTechnicalResource.cs
@@ -0,0 +1,5 @@
+﻿namespace HelpTechService.Payment.Interfaces.REST.Resources.QrTechnical
+{
+    public record QrTechnicalResource(int Id, string TechnicalId,
+        DateTime RegistrationDate, string QrUrl, string State);
+}
---
+++ HelpTechService/Payment/Interfaces/REST/Transform/QrTechnical/AddQrTechnicalCommandFromResourceAssembler.cs
@@ -0,0 +1,14 @@
+﻿using HelpTechService.Payment.Domain.Model.Commands.QrTechnical;
+using HelpTechService.Payment.Domain.Model.ValueObjects.QrTechnical;
+using HelpTechService.Payment.Interfaces.REST.Resources.QrTechnical;
+
+namespace HelpTechService.Payment.Interfaces.REST.Transform.QrTechnical
+{
+    public class AddQrTechnicalCommandFromResourceAssembler
+    {
+        public static AddQrTechnicalCommand ToCommandFromResource
+            (AddQrTechnicalResource resource) =>
+            new(resource.TechnicalId, resource.QrUrl,
+                EQrTechnicalState.ACTIVO);
+    }
+}
