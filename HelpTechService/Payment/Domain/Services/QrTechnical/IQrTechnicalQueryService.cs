3d8e206e1bb9421aeb555bf7ee3f0f6a8fbc0793
Mauricio Rojas <mauriciorojasanchez@gmail.com>
2025-09-24 15:56:14 -0500

    feat(payment): added application.

---
+++ HelpTechService/Payment/Domain/Services/QrTechnical/IQrTechnicalQueryService.cs
@@ -0,0 +1,10 @@
+﻿using HelpTechService.Payment.Domain.Model.Queries.QrTechnical;
+
+namespace HelpTechService.Payment.Domain.Services.QrTechnical
+{
+    public interface IQrTechnicalQueryService
+    {
+        Task<IEnumerable<Model.Aggregates.QrTechnical>> Handle
+            (GetQrTechnicalByTechnicalId query);
+    }
+}
