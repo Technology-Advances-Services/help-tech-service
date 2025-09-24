f9e9cc602fc04c8354a72ba64946fd2a9914c15a
Mauricio Rojas <mauriciorojasanchez@gmail.com>
2025-09-24 15:48:56 -0500

    feat(payment): added add qr technical command.

---
+++ HelpTechService/Payment/Domain/Model/Commands/QrTechnical/AddQrTechnicalCommand.cs
@@ -0,0 +1,7 @@
+﻿using HelpTechService.Payment.Domain.Model.ValueObjects.QrTechnical;
+
+namespace HelpTechService.Payment.Domain.Model.Commands.QrTechnical
+{
+    public record AddQrTechnicalCommand(string TechnicalId,
+        string QrUrl, EQrTechnicalState QrTechnicalState);
+}
