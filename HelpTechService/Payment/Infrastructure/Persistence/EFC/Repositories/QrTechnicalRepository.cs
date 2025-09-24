cf243b62a671681a92a42eefdd4c2c35ee214e40
Mauricio Rojas <mauriciorojasanchez@gmail.com>
2025-09-24 15:57:05 -0500

    feat(payment): added application.

---
+++ HelpTechService/Payment/Infrastructure/Persistence/EFC/Repositories/QrTechnicalRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HelpTechService.Payment.Domain.Model.Aggregates;
+using HelpTechService.Payment.Domain.Repositories;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using HelpTechService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace HelpTechService.Payment.Infrastructure.Persistence.EFC.Repositories
+{
+    internal class QrTechnicalRepository
+        (HelpTechContext context) :
+        BaseRepository<QrTechnical>(context),
+        IQrTechnicalRepository
+    {
+        public async Task<IEnumerable<QrTechnical>> FindByTechnicalIdAsync
+            (int technicalId) => await Context.Set<QrTechnical>()
+            .Where(q => q.TechnicalsId == technicalId && q.State == "ACTIVO")
+            .AsNoTrackingWithIdentityResolution().ToListAsync();
+    }
+}
