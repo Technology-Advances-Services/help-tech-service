3dae271cea677d1e7555d55703dc973a4b518b4d
Mauricio Rojas <mauriciorojasanchez@gmail.com>
2025-09-24 15:50:24 -0500

    feat(payment): added model complete.

--- HelpTechService/Payment/Domain/Model/Aggregates/QrTechnical.cs
+++ HelpTechService/Payment/Domain/Model/Aggregates/QrTechnical.cs
@@ -1,5 +1,7 @@
-﻿using TimeZoneConverter;
-using HelpTechService.IAM.Domain.Model.Aggregates;
+﻿using HelpTechService.IAM.Domain.Model.Aggregates;
+using HelpTechService.Payment.Domain.Model.Commands.QrTechnical;
+using HelpTechService.Payment.Domain.Model.ValueObjects.QrTechnical;
+using TimeZoneConverter;
 
 namespace HelpTechService.Payment.Domain.Model.Aggregates
 {
@@ -19,6 +21,25 @@ namespace HelpTechService.Payment.Domain.Model.Aggregates
             this.TechnicalsId = 0;
             this.QrUrl = string.Empty;
             this.State = string.Empty;
+        }
+        public QrTechnical(string technicalId, string qrUrl,
+            EQrTechnicalState qrTechnicalState)
+        {
+            this.TechnicalsId = int.TryParse
+                (technicalId, out int technicalsId) != false ?
+                int.Parse(technicalsId.ToString().TrimStart('0')) : 0;
+            this.RegistrationDate = GetLimaNow();
+            this.QrUrl = qrUrl;
+            this.State = qrTechnicalState.ToString();
+        }
+        public QrTechnical(AddQrTechnicalCommand command)
+        {
+            this.TechnicalsId = int.TryParse
+                (command.TechnicalId, out int technicalsId) != false ?
+                int.Parse(technicalsId.ToString().TrimStart('0')) : 0;
+            this.RegistrationDate = GetLimaNow();
+            this.QrUrl = command.QrUrl;
+            this.State = command.QrTechnicalState.ToString();
         }
 
         public static DateTime GetLimaNow()
