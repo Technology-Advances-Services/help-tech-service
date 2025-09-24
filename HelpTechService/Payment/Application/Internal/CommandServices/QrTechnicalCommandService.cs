0bd867477aa5a31f3678a17cdcfd61b05bb55f14
Mauricio Rojas <mauriciorojasanchez@gmail.com>
2025-09-24 15:49:52 -0500

    feat(payment): added qr technical command.

---
+++ HelpTechService/Payment/Application/Internal/CommandServices/QrTechnicalCommandService.cs
@@ -0,0 +1,28 @@
+﻿using HelpTechService.Payment.Domain.Model.Commands.QrTechnical;
+using HelpTechService.Payment.Domain.Repositories;
+using HelpTechService.Payment.Domain.Services.QrTechnical;
+using HelpTechService.Shared.Domain.Repositories;
+
+namespace HelpTechService.Payment.Application.Internal.CommandServices
+{
+    internal class QrTechnicalCommandService
+        (IQrTechnicalRepository qrTechnicalRepository,
+        IUnitOfWork unitOfWork) :
+        IQrTechnicalCommandService
+    {
+        public async Task<bool> Handle
+            (AddQrTechnicalCommand command)
+        {
+            try
+            {
+                await qrTechnicalRepository
+                    .AddAsync(new(command));
+
+                await unitOfWork.CompleteAsync();
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
